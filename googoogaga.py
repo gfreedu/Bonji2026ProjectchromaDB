4db47390fe884d26da14cace08f67d5816c9d277
gfreedu <theamazewichusesgfreedy@gmail.com>
2026-07-02 13:51:39 +0530

    Actually added chromaDB using MiniLM L6 v2

--- googoogaga.py
+++ googoogaga.py
@@ -1,4 +1,21 @@
-print("hello world.")
+import chromadb
+chroma_client = chromadb.Client()
 
-for i in range(1,10):
-    print("Daddy Chill!!")
+collection = chroma_client.create_collection(name="googoogaagas")
+
+collection.add(
+    documents=["A small furry animal that is very stupid and keeps making bad decisions.",
+    "A hairless animal that is very intelligent and solves difficult problems."],
+    metadatas=[
+        {'weight':'light','fur':True},
+        {'weight':'heavy','fur':False}  
+        ],
+    ids=['stupid1','stupid2']
+)
+
+results = collection.query(
+    query_texts=['furless','without fur','bald'],
+    n_results=1
+)
+
+print('\n The results are:',results)
