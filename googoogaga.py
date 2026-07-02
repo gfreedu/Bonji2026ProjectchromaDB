9b518ef56847a646ba63e5044c823fd43582a8c7
gfreedu <theamazewichusesgfreedy@gmail.com>
2026-07-02 16:54:00 +0530

    Added test data(pls ignore the test data pls) and got json file reading to work got chromadb to run on given test data

--- googoogaga.py
+++ googoogaga.py
@@ -9,27 +9,33 @@ with open("data/products.json",'r') as f:
     data = json.load(f)
 
 def setData():
-    pass
+    for i in data:
+        documentIds.append(i['id'])
+        docs.append(f"{i["name"]} is a {i["product_type"]} made by {i['brand']}.It belongs to the {i['category']} category.Description: {i['description']}")
+        metaDatas.append(
+            {"name":i["name"],
+             'brand':i['brand'],
+             'category':i['category'],
+             'product_type':i['product_type'],
+             'price':i['price']
+             })
 
-print(data.items())
+setData()
 
 chroma_client = chromadb.Client()
 
 collection = chroma_client.create_collection(name="googoogaagas")
 
 collection.add(
-    documents=["A small furry animal that is very stupid and keeps making bad decisions.",
-    "A hairless animal that is very intelligent and solves difficult problems."],
-    metadatas=[
-        {'weight':'light','fur':True},
-        {'weight':'heavy','fur':False}  
-        ],
-    ids=['stupid1','stupid2']
+    documents = docs,
+    metadatas = metaDatas,
+    ids = documentIds
 )
 
 results = collection.query(
-    query_texts=['furless','without fur','bald'],
-    n_results=1
+    query_texts=["hydrating skincare", "products for damaged hair", "vitamin c serum", "refreshing face mist", "Hatsune Miku", "winter hand cream", "oily hair shampoo"],
+    n_results=3
 )
 
-print('\n The results are:',results)
+for x in results['ids']:
+    print(x, end='\n\n')
